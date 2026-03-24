b1973d1474cfe9361cd9cbaf88590f127a413baf
Bastian Kuberek <bkuberek@gmail.com>
2026-03-24 13:54:04 -0400

    fix: resolve Keycloak startup failures on Coolify deployment
    - KC_HOSTNAME must be full URL with https:// scheme (KC26 requirement
      when KC_HOSTNAME_BACKCHANNEL_DYNAMIC=true)
    - Add KC_HOSTNAME_FQDN for Traefik Host() label (bare hostname)
    - Init script creates both keycloak_db and knowledge_keycloak databases
    - Document KC_HOSTNAME format requirement in .env.example

--- docker/postgres/init-keycloak-db.sql
+++ docker/postgres/init-keycloak-db.sql
@@ -1,3 +1,7 @@
--- Create separate database for Keycloak
+-- Create separate databases for Keycloak
+-- Local dev uses keycloak_db, production (Coolify) uses knowledge_keycloak
 CREATE DATABASE keycloak_db;
 GRANT ALL PRIVILEGES ON DATABASE keycloak_db TO knowledge;
+
+CREATE DATABASE knowledge_keycloak;
+GRANT ALL PRIVILEGES ON DATABASE knowledge_keycloak TO knowledge;
